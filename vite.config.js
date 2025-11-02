00fe8ba2753c13cf70194d5560da16cb163a43e1
Martin Droguett <martin.dtondro@gmail.com>
2025-11-02 00:12:50 -0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { resolve } from 'path'
+//a
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|avif/i.test(ext)) {
+            return `assets/images/[name][extname]`;
+          }
+          return `assets/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
+      }
+    }
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@images': resolve(__dirname, 'public/Imagenes')
+    }
+  }
 })
